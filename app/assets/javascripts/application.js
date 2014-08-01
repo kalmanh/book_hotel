ca3e49a31026d3a3f7e1b852ffa930b0ce7a375c
Kalman Hazins <kalmanh@gmail.com>
2014-08-01 14:08:17 -0400

    Autocomplete and datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
@@ -17,4 +5,15 @@
 
 $(function(){
   $("#notice").fadeOut(5000);
+
+  var availableCities = [
+    "Baltimore", "New York", "Columbia", "Laurel",
+    "Salt Lake", "Silver Spring", "Washington",
+    "Philadelphia", "Las Vegas"
+  ];
+
+  $("#city").autocomplete({source: availableCities});
+
+  $("#date_in").datepicker({dateFormat: "yy-mm-dd"});
+
 });
